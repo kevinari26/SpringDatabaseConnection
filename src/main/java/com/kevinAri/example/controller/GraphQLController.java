aa9161692d5ca54e07ed24d2df4045642cd76284
kevinari26 <kevin_arisaputra@yahoo.com>
2023-10-07 18:13:22 +0700

    transactional example

--- src/main/java/com/kevinAri/example/controller/GraphQLController.java
+++ src/main/java/com/kevinAri/example/controller/GraphQLController.java
@@ -2,12 +2,17 @@ package com.kevinAri.example.controller;
 
 import com.kevinAri.example.model.entity.TestEntity;
 import com.kevinAri.example.model.repository.TestRepo;
+import com.kevinAri.example.service.AppService;
 import com.kevinAri.example.util.CriteriaParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
@@ -20,24 +25,6 @@ public class GraphQLController {
     // API GraphQL
     @Autowired
     private TestRepo repo;
-
-    @QueryMapping
-    public List<TestEntity> persons () {
-        return repo.findAll();
-    }
-
-    @QueryMapping
-    public Object personById (@Argument Long id) {
-        return repo.findById(id);
-    }
-
-    @QueryMapping
-    public List<TestEntity> personsFilter (@Argument String filterQuery) {
-        CriteriaParser<TestEntity> critParser = new CriteriaParser<>();
-        critParser.setDateTimeArr(Collections.singletonList("createdDate"));
-        Specification<TestEntity> specs = critParser.parseRecursively(filterQuery);
-        return repo.findAll(specs);
-    }
 
     // payload example
     /*
@@ -60,4 +47,38 @@ public class GraphQLController {
       }
     }
     */
+    @QueryMapping
+    public List<TestEntity> persons () {
+        return repo.findAll();
+    }
+
+    @QueryMapping
+    public Object personById (@Argument Long id) {
+        return repo.findById(id);
+    }
+
+    @QueryMapping
+    public List<TestEntity> personsFilter (@Argument String filterQuery) {
+        CriteriaParser<TestEntity> critParser = new CriteriaParser<>();
+        critParser.setDateTimeArr(Collections.singletonList("createdDate"));
+        Specification<TestEntity> specs = critParser.parseRecursively(filterQuery);
+        return repo.findAll(specs);
+    }
+
+
+    @Autowired
+    AppService appService;
+
+    @GetMapping(value = "/test-transactional", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Object hello() {
+        try {
+            appService.execute2();
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body("ok");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("error");
+        }
+    }
 }
